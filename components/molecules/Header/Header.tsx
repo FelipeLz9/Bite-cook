88fecb94c78061b71c4873c963441dcfb865c5e6
Pabs125 <pablon.ocampol@autonoma.edu.co>
2024-10-07 13:09:56 -0500

    Mejoramiento del header

--- components/molecules/Header/Header.tsx
+++ components/molecules/Header/Header.tsx
@@ -1,25 +1,26 @@
 import Link from "next/link";
+import "../Header/Header.css"; // Asegúrate de que la ruta es correcta
 
 export const Header = () => {
-	return (
-		<header className="header">
-		<nav className="nav">
-			<ul className="navlist">
-			<li>
-				<Link href="/">Home</Link>
-			</li>
-			<li>
-				<Link href="/about">About us</Link>
-			</li>
-			<li>
-				<Link href="/contact">Contact</Link>
-			</li>
-			</ul>
-		</nav>
-		<div className="sesion">
-			<Link href="/login">Login</Link>
-			<Link href="/register">Register</Link>
-		</div>
-		</header>
-	);
+  return (
+    <header className="header">
+      <nav className="nav">
+        <ul className="navlist">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About us</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="sesion">
+        <Link href="/login">Login</Link>
+        <Link href="/register">Register</Link>
+      </div>
+    </header>
+  );
 };
